e00f9574cedab619f7f2194763903c880e1fd8e9
BahirHakimy <bahirhakimy2020@gmail.com>
2023-10-26 11:27:20 +0430

    Add the smart buttons and pages

--- volunteer_management/__manifest__.py
+++ volunteer_management/__manifest__.py
@@ -1,36 +1,29 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Volunteer Management System",
-
-    'summary': """
+    "name": "Volunteer Management System",
+    "summary": """
         Volunteer Management System
         """,
-
-    'description': """
+    "description": """
         Long description of module's purpose
     """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
+    "author": "My Company",
+    "website": "https://www.yourcompany.com",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-    'sequence': '-1000',
-    'application': True,
-
+    "category": "Uncategorized",
+    "version": "0.1",
+    "sequence": "-1000",
+    "application": True,
     # any module necessary for this one to work correctly
-    'depends': ['base'],
-
+    "depends": ["base"],
     # always loaded
-    'data': [
-        'security/ir.model.access.csv',
-        'views/volunteers.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "views/volunteers.xml",
+        "views/volunteers_certificates.xml",
+        "views/volunteers_deployments.xml",
+        "views/volunteers_trainings.xml",
     ],
 }
--- volunteer_management/models/volunteer.py
+++ volunteer_management/models/volunteer.py
@@ -4,58 +4,59 @@ from odoo import models, fields, api
 
 
 class Volunteer(models.Model):
-    _name = 'vms.volunteer'
-    _description = 'Volunteers'
+    _name = "vms.volunteer"
+    _description = "Volunteers"
 
     name = fields.Char()
     father_name = fields.Char()
     tazkira_number = fields.Char()
     dob = fields.Date()
     age = fields.Integer()
-    
+
     mobile = fields.Char()
     photo = fields.Image()
+
+    gender = fields.Selection([("male", "male"), ("female", "female")])
+    marital_status = fields.Selection(
+        [
+            ("S", "Single"),
+            ("M", "Married"),
+            ("D", "Divorced"),
+            ("W", "Widowed"),
+            ("E", "Engaged"),
+        ]
+    )
+    blood_group = fields.Selection(
+        [
+            ("A+", "A Positive"),
+            ("A-", "A Negative"),
+            ("B+", "B Positive"),
+            ("B-", "B Negative"),
+            ("AB+", "AB Positive"),
+            ("AB-", "AB Negative"),
+            ("O+", "O Positive"),
+            ("O-", "O Negative"),
+        ]
+    )
+
+    related_user = fields.Many2one("res.users")
+
+    state = fields.Selection([("approved", "Approved"), ("rejected", "Rejected")])
+
+    permanent_province = fields.Many2one("vms.province", string="Permanent Province")
+    permanent_district = fields.Many2one("vms.district", string="Permanent District")
+    current_province = fields.Many2one("vms.province", string="Current Province")
+    current_district = fields.Many2one("vms.district", string="Current District")
+    type_of_volunteer = fields.Many2one(
+        "vms.volunteer.type", string="Type of Volunteer"
+    )
+    occupation = fields.Many2one("vms.occupation", string="Occupation")
     specialization = fields.Char()
-
-    gender = fields.Selection([('male', 'male'), ('female', 'female')])
-    marital_status = fields.Selection([
-        ("S", "Single"),
-        ("M", "Married"),
-        ("D", "Divorced"),
-        ("W", "Widowed"),
-        ("E", "Engaged"),
-    ])
-    blood_group = fields.Selection([
-        ("A+", "A Positive"),
-        ("A-", "A Negative"),
-        ("B+", "B Positive"),
-        ("B-", "B Negative"),
-        ("AB+", "AB Positive"),
-        ("AB-", "AB Negative"),
-        ("O+", "O Positive"),
-        ("O-", "O Negative"),
-    ])
-    # state = fields.Selection([])
-
-    # occupation = fields.Many2one()
-    # languages = fields.Many2many()
-    # disabilities = fields.One2many()
-    # type_of_volunteer = fields.Many2one()
-    # permanent_province = fields.Many2one()
-    # permanent_district = fields.Many2one()
-    # current_province = fields.Many2one()
-    # current_district = fields.Many2one()
-    # emergency_contacts = fields.One2many()
-    # education_degree = fields.Many2one()
-    # documents = fields.One2many()
-    # related_user = fields.Many2one()
-    # deployments = fields.One2many()
-    # certificates = fields.One2many()
-    # trainings = fields.One2many()
-
-    partner_id = fields.Many2one('res.partner')
-
+    education_degree = fields.Many2one(
+        "vms.education.degree", string="Education Degree"
+    )
     place_of_work = fields.Char()
+    languages = fields.Many2many("vms.language", string="Languages")
     number_of_children = fields.Integer()
     driver_license = fields.Boolean()
     permanent_village = fields.Char()
@@ -64,3 +65,143 @@ class Volunteer(models.Model):
     current_school_mosque = fields.Char()
     university_school = fields.Char()
     comments = fields.Text()
+    disabilities = fields.One2many(
+        "vms.disability", "volunteer_id", string="Disabilities"
+    )
+    emergency_contacts = fields.One2many(
+        "vms.emergency.contact", "volunteer_id", string="Emergency Contacts"
+    )
+    documents = fields.One2many("vms.document", "volunteer_id", string="Documents")
+
+    # not used ------------
+    deployments = fields.One2many(
+        "vms.deployment", "volunteer_id", string="Deployments"
+    )
+    certificates = fields.One2many(
+        "vms.certificate", "volunteer_id", string="Certificates"
+    )
+    trainings = fields.One2many("vms.training", "volunteer_id", string="Trainings")
+
+    def open_deployments(self):
+        for rec in self:
+            return {
+                "name": "Deployments",
+                "type": "ir.actions.act_window",
+                "view_mode": "tree,form",
+                "res_model": "vms.deployment",  # Replace with your actual model name
+                "domain": [("volunteer_id", "=", rec.id)],
+            }
+
+    # Action function to open Certificates
+    def open_certificates(self):
+        for rec in self:
+            return {
+                "name": "Certificates",
+                "type": "ir.actions.act_window",
+                "view_mode": "tree,form",
+                "res_model": "vms.certificate",  # Replace with your actual model name
+                "domain": [("volunteer_id", "=", rec.id)],
+            }
+
+    # Action function to open Trainings
+    def open_trainings(self):
+        for rec in self:
+            return {
+                "name": "Trainings",
+                "type": "ir.actions.act_window",
+                "view_mode": "tree,form",
+                "res_model": "vms.training",  # Replace with your actual model name
+                "domain": [("volunteer_id", "=", rec.id)],
+            }
+
+
+class Occupation(models.Model):
+    _name = "vms.occupation"
+    _description = "Occupation"
+
+    name = fields.Char(string="Occupation", required=True)
+
+
+class Language(models.Model):
+    _name = "vms.language"
+    _description = "Language"
+
+    name = fields.Char(string="Language", required=True)
+
+
+class Disability(models.Model):
+    _name = "vms.disability"
+    _description = "Disability"
+
+    name = fields.Char(string="Disability", required=True)
+    volunteer_id = fields.Many2one("vms.volunteer", string="Volunteer")
+
+
+class VolunteerType(models.Model):
+    _name = "vms.volunteer.type"
+    _description = "Type of Volunteer"
+
+    name = fields.Char(string="Type of Volunteer", required=True)
+
+
+class Province(models.Model):
+    _name = "vms.province"
+    _description = "Province"
+
+    name = fields.Char(string="Province", required=True)
+
+
+class District(models.Model):
+    _name = "vms.district"
+    _description = "District"
+
+    name = fields.Char(string="District", required=True)
+
+
+class EmergencyContact(models.Model):
+    _name = "vms.emergency.contact"
+    _description = "Emergency Contact"
+
+    name = fields.Char(string="Name", required=True)
+    phone = fields.Char(string="Phone")
+    volunteer_id = fields.Many2one("vms.volunteer", string="Volunteer")
+
+
+class EducationDegree(models.Model):
+    _name = "vms.education.degree"
+    _description = "Education Degree"
+
+    name = fields.Char(string="Education Degree", required=True)
+
+
+class Document(models.Model):
+    _name = "vms.document"
+    _description = "Document"
+
+    name = fields.Char(string="Name", required=True)
+    volunteer_id = fields.Many2one("vms.volunteer", string="Volunteer")
+    document_file = fields.Binary(string="Document")
+
+
+class Deployment(models.Model):
+    _name = "vms.deployment"
+    _description = "Deployment"
+
+    name = fields.Char(string="Deployment", required=True)
+    volunteer_id = fields.Many2one("vms.volunteer", string="Volunteer")
+
+
+class Certificate(models.Model):
+    _name = "vms.certificate"
+    _description = "Certificate"
+
+    name = fields.Char(string="Certificate", required=True)
+    volunteer_id = fields.Many2one("vms.volunteer", string="Volunteer")
+
+
+class Training(models.Model):
+    _name = "vms.training"
+    _description = "Training"
+
+    name = fields.Char(string="Training", required=True)
+    volunteer_id = fields.Many2one("vms.volunteer", string="Volunteer")
