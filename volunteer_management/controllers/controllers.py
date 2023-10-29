9801e2276bc4720aca35cdb83ceabce25e5ec92c
iqbal-elham <iqbal.elham7@gmail.com>
2023-10-29 14:04:03 +0430

    Add the portal path

--- volunteer_management/controllers/controllers.py
+++ volunteer_management/controllers/controllers.py
@@ -4,8 +4,8 @@ from odoo.http import request
 
 
 class VolunteerPortal(CustomerPortal):
-
-    def _prepare_home_portal_values(self, counters):
-        rtn = super()._prepare_home_portal_values(counters)
+    pass
+    # def _prepare_home_portal_values(self, counters):
+    #     rtn = super()._prepare_home_portal_values(counters)
         # rtn['volunteer_count'] = request.env['vms.volunteer'].search_count([])
-        return rtn
+        # return rtn
